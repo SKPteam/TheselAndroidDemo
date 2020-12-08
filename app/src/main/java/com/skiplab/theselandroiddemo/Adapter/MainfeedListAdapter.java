5ecb2ec6a3b07e9a4391aa98fcb16d5f932963d0
SKPnation <ayomideseaz@gmail.com>
2020-12-08 18:37:32 +0100

    working on the mainfeedListAdapter and the post data model

---
+++ app/src/main/java/com/skiplab/theselandroiddemo/Adapter/MainfeedListAdapter.java
@@ -0,0 +1,6 @@
+package com.skiplab.theselandroiddemo.Adapter;
+
+import android.widget.ArrayAdapter;
+
+public class MainfeedListAdapter extends ArrayAdapter<> {
+}
---
+++ app/src/main/java/com/skiplab/theselandroiddemo/models/Post.java
@@ -0,0 +1,8 @@
+package com.skiplab.theselandroiddemo.models;
+
+public class Post {
+    String pId, pDescription, pImage, pLikes, pComments, pTime, uid, uName, uEmail, uDp, uMood, pCategory, tags;
+
+
+
+}
